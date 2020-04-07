5c3dbc3666bb26cf096702c09ca8a7458826743a
@danielfratia <daniel.fratila@yahoo.com>
2020-04-07 17:13:53 +0300

    Initial Commit

---
+++ Stam Acasa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stam Acasa
+//
+//  Created by Macbook on 4/7/20.
+//  Copyright © 2020 IOs apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stam AcasaTests/Stam_AcasaTests.swift
@@ -0,0 +1,34 @@
+//
+//  Stam_AcasaTests.swift
+//  Stam AcasaTests
+//
+//  Created by Macbook on 4/7/20.
+//  Copyright © 2020 IOs apps. All rights reserved.
+//
+
+import XCTest
+@testable import Stam_Acasa
+
+class Stam_AcasaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
