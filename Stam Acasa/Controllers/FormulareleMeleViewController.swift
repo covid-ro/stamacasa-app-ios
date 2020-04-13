76d6e69da99bebbaa99e62da83e35a00c929ebe9
sebastian.cotarlici <sebastian.cotarlici@digisoftware.ro>
2020-04-14 00:58:53 +0300

    Tabel simptome

--- Stam Acasa/Controllers/FormulareleMeleViewController.swift
+++ Stam Acasa/Controllers/FormulareleMeleViewController.swift
@@ -10,7 +10,33 @@ import Foundation
 import UIKit
 
 class FormulareleMeleViewController: UIViewController {
-
+    
+    @IBOutlet weak var Field_1: UILabel!
+    @IBOutlet weak var Field_2: UILabel!
+    @IBOutlet weak var Field_3: UILabel!
+    @IBOutlet weak var Field1_1: UILabel!
+    @IBOutlet weak var Field1_2: UILabel!
+    @IBOutlet weak var Field1_3: UILabel!
+    @IBOutlet weak var Field2_1: UILabel!
+    @IBOutlet weak var Field2_2: UILabel!
+    @IBOutlet weak var Field2_3: UILabel!
+    @IBOutlet weak var Field3_1: UILabel!
+    @IBOutlet weak var Field3_2: UILabel!
+    @IBOutlet weak var Field3_3: UILabel!
+    @IBOutlet weak var Field4_1: UILabel!
+    @IBOutlet weak var Field4_2: UILabel!
+    @IBOutlet weak var Field4_3: UILabel!
+    @IBOutlet weak var Field5_1: UILabel!
+    @IBOutlet weak var Field5_2: UILabel!
+    @IBOutlet weak var Field5_3: UILabel!
+    @IBOutlet weak var Field6_1: UILabel!
+    @IBOutlet weak var Field6_2: UILabel!
+    @IBOutlet weak var Field6_3: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
