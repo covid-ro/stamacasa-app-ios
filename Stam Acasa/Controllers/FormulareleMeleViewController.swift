d67d55263bef5fc81e867aba93af6d47515fa395
sebastian.cotarlici <sebastian.cotarlici@digisoftware.ro>
2020-04-14 21:29:20 +0300

    add formularele mele vc

--- Stam Acasa/Controllers/FormulareleMeleViewController.swift
+++ Stam Acasa/Controllers/FormulareleMeleViewController.swift
@@ -11,34 +11,77 @@ import UIKit
 
 class FormulareleMeleViewController: UIViewController {
     
-    @IBOutlet weak var Field_1: UILabel!
-    @IBOutlet weak var Field_2: UILabel!
-    @IBOutlet weak var Field_3: UILabel!
-    @IBOutlet weak var Field1_1: UILabel!
-    @IBOutlet weak var Field1_2: UILabel!
-    @IBOutlet weak var Field1_3: UILabel!
-    @IBOutlet weak var Field2_1: UILabel!
-    @IBOutlet weak var Field2_2: UILabel!
-    @IBOutlet weak var Field2_3: UILabel!
-    @IBOutlet weak var Field3_1: UILabel!
-    @IBOutlet weak var Field3_2: UILabel!
-    @IBOutlet weak var Field3_3: UILabel!
-    @IBOutlet weak var Field4_1: UILabel!
-    @IBOutlet weak var Field4_2: UILabel!
-    @IBOutlet weak var Field4_3: UILabel!
-    @IBOutlet weak var Field5_1: UILabel!
-    @IBOutlet weak var Field5_2: UILabel!
-    @IBOutlet weak var Field5_3: UILabel!
-    @IBOutlet weak var Field6_1: UILabel!
-    @IBOutlet weak var Field6_2: UILabel!
-    @IBOutlet weak var Field6_3: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var istoricView: UIView!
+    @IBOutlet weak var columnsView: UIView!
+    @IBOutlet weak var istoricHeight: NSLayoutConstraint!
     
-    
+    @IBOutlet weak var scrollviewHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //istoricHeight.constant = 0;
+        //self.view.layoutIfNeeded()
+        //istoricView.removeFromSuperview()
+        drawIstoric()
+        
+        //scrollView.contentSize = CGSize(width: 375, height: 1057)
+        scrollviewHeight.constant = 1057
+        scrollviewHeight.priority = .defaultHigh
+        self.view.layoutIfNeeded()
     }
-
+    
+    func drawIstoric() {
+           super.viewDidLoad()
+           
+           var responses : [ResponseData]?
+           responses = []
+           
+           if let encodedData = UserDefaults.standard.object(forKey: "resps") as? Data {
+               let decoder = JSONDecoder()
+               if let rsx = try? decoder.decode([ResponseData].self, from: encodedData) {
+                   responses = rsx
+               }
+           }
+           
+           for report in 0...responses!.count-1 where report < 3 {
+               
+               var columnView = UIView.init(frame: CGRect.init(x: report*60, y: 0, width: 60, height: 420))
+               
+               var dataLabel = UILabel(frame: CGRect(x:0, y:0, width:60, height:60))
+               dataLabel.textAlignment = NSTextAlignment.center
+               dataLabel.text = responses![report].date
+               dataLabel.numberOfLines = 1
+               columnView.addSubview(dataLabel)
+               
+               var separator = UIView.init(frame: CGRect.init(x: 0, y: 59, width: 60, height: 1))
+               separator.backgroundColor = .lightGray
+               columnView.addSubview(separator)
+               
+               var rsps = responses![report].responses! as [ResponseData.Answer]
+               
+               for i in 1...6 {
+                   var field = "NU" as String
+                   
+                   for ans in 0...rsps.count-1{
+                       if rsps[ans].question_id == 10 && rsps[ans].answer_id == i {
+                           field = "DA"
+                       }
+                   }
+                   
+                   var label = UILabel(frame: CGRect(x:0, y:i*60, width:60, height:60))
+                   label.textAlignment = NSTextAlignment.center
+                   label.font = UIFont.italicSystemFont(ofSize: 14)
+                   label.text = field
+                   if field=="DA" {
+                       label.textColor = UIColor.red
+                   }
+                   columnView.addSubview(label)
+               }
+               columnsView.addSubview(columnView)
+           }
+           //scrollView.contentSize = CGSize.init(width: responses!.count * 60, height: 420)
+       }
 }
+
