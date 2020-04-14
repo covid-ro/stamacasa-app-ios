905b8e872378efcf8f67e82189d46d8025371ea4
sebastian.cotarlici <sebastian.cotarlici@digisoftware.ro>
2020-04-14 13:33:20 +0300

    istoricCompletVc

---
+++ Stam Acasa/Controllers/IstoricCompletViewController.swift
@@ -0,0 +1,20 @@
+//
+//  IstoricCompletViewController.swift
+//  Stam Acasa
+//
+//  Created by Sebi on 14/04/2020.
+//  Copyright © 2020 IOs apps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IstoricCompletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
